c094c8315a014e1fa686de837e8619f4dfad1605
Marktwennn <danilasagirov2001@gmail.com>
2024-04-14 17:57:49 +0400

    Update7

---
+++ Assets/Scripts/ReplenishmentOfAmmunition.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class ReplenishmentOfAmmunition : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(false);
+        }
+    }
+
+}
