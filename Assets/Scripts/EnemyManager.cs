46e50b4cfa50a49fc27124064142f57650531e77
Victor Karpov <Nilafe4@gmail.com>
2024-04-14 22:59:16 +0500

    Create Nav enemy

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-
     [SerializeField] private Transform[] m_Spawne;
-    [SerializeField] private GameObject m_EnenamyPrefab;
-    // Start is called before the first frame update
+    [SerializeField] private GameObject m_EnemyPrefab;
+    [SerializeField] private int m_EnemyCount = 5; // Количество врагов для спауна
+    [SerializeField] private float m_SpawnInterval = 2f; // Интервал между спаунами
+
+    private int m_CurrentSpawned = 0;
+    private float m_Timer = 0f;
+
     void Start()
     {
-        
+        // Запускаем спаун врагов
+        InvokeRepeating("SpawnEnemies", 0f, m_SpawnInterval);
     }
 
-    void OnEnable()
+    void SpawnEnemies()
     {
-          
-    }
+        // Проверяем, что мы еще не заспаунили все врагов
+        if (m_CurrentSpawned < m_EnemyCount)
+        {
+            // Выбираем случайную точку спауна
+            int randomPoint = Random.Range(0, m_Spawne.Length);
 
-    void SpawnerEnemy()
-    {
-        Instantiate(m_EnenamyPrefab, m_Spawne[0].transform.position, Quaternion.identity);
-    }
+            // Спауним врага
+            Instantiate(m_EnemyPrefab, m_Spawne[randomPoint].position, Quaternion.identity);
 
+            // Увеличиваем счетчик заспауненных врагов
+            m_CurrentSpawned++;
+        }
+        else
+        {
+            // Если все враги заспаунились, отменяем повторяющийся вызов
+            CancelInvoke("SpawnEnemies");
+        }
+    }
 }
