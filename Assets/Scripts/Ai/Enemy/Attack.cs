c6d1c2da76655eeddfc9cb80552febdc9160f880
gooddayx <amazingspace@ya.ru>
2024-04-26 01:56:39 +0500

    edit_UI

--- Assets/Scripts/Ai/Enemy/Attack.cs
+++ Assets/Scripts/Ai/Enemy/Attack.cs
@@ -4,13 +4,30 @@ public class Attack: MonoBehaviour
 {
     [SerializeField] private int damage = 10;
     [SerializeField] private Animator animator;
-   
+    [SerializeField] private GameObject particle;
+
+
+    private void OnEnable()
+    {
+        particle.SetActive(false);
+    }
+
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.TryGetComponent<Health>(out Health hp))
-        { 
-            hp.OnTakeDameg?.Invoke(damage);
-            animator.SetBool("StateAttack", true);
+        {
+            if(particle == null)
+            {
+                hp.OnTakeDameg?.Invoke(damage);
+                animator.SetBool("StateAttack", true);
+            }
+            else
+            {
+                hp.OnTakeDameg?.Invoke(damage);
+
+                particle.SetActive(true);
+                animator.SetBool("StateAttack", true);
+            }
         }
     }
 
